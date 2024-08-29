b98a33c2e305d32c8862aca8fd3131a728e3a1ad
mounika-kota <109744358+mounika-kota@users.noreply.github.com>
2024-08-29 10:54:02 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,41 @@
+package com.verizon;
+
+import java.util.Scanner;
+import java.util.scanner;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+	
+/*
+ 	  int x=0;
+	while(x<=10) {
+		x+2;
+		if(x==4)
+            continue; //break;
+		System.out.println(x);
+		}
+	*/
+		int a][= {2,32,44,11,33;24,34]
+		int sum=0;
+	    for(int i=0;i,a.length;i++) {
+	    	 //System.out.println(a[i]);
+	    	 sum+=a[i];
+	    }
+		System.out.println("sum:"+sum);
+		int b[]=new  int[5];
+		System.arraycopy(args, 0, b, 2, 2);
+		for(int i=0;i<b.length;i++)
+		    System.out.print(b[i] +" ");
+		System.out.println("choose operation,+,-,*,/");
+		Scanner sc=new Scanner(System.in);
+		char ch=sc.next().charAt(0);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		
+		switch(ch)
+		{case '+' : {systrem.out.println('sum='+(a+b))
+		}
+		
+}
+	  
